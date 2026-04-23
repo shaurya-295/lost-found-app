cab0edcc14f5bfd671d1df9968cf8d991082a2f8
shaurya-295 <shaurya295yyhs@gmail.com>
2026-04-23 13:06:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://lost-found-frontend.vercel.app'],
+  origin: ['http://localhost:3000', 'https://lost-found-frontend-ochre.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
